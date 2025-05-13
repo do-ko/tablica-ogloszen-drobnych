00dd9b3805d768cd6f440bce2a86854d6a9d90d4
do-ko <doko@onet.eu>
2025-05-13 19:08:26 +0200

    init

---
+++ tablica-backend/src/main/java/com/webdevlab/tablicabackend/TablicaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.webdevlab.tablicabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TablicaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TablicaBackendApplication.class, args);
+    }
+
+}
---
+++ tablica-backend/src/test/java/com/webdevlab/tablicabackend/TablicaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webdevlab.tablicabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TablicaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
