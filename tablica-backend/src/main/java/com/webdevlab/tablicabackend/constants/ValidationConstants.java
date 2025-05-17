0369ad6415bb330b795a7617748bec1ed551689c
do-ko <doko@onet.eu>
2025-05-17 21:29:37 +0200

    constants types adjustment

--- tablica-backend/src/main/java/com/webdevlab/tablicabackend/constants/ValidationConstants.java
+++ tablica-backend/src/main/java/com/webdevlab/tablicabackend/constants/ValidationConstants.java
@@ -6,7 +6,7 @@ public final class ValidationConstants {
     }
 
     //    USER
-    public static final Integer USER_USERNAME_MAX_LENGTH = 64;
-    public static final Integer USER_PASSWORD_MAX_LENGTH = 64;
-    public static final Integer USER_PASSWORD_MIN_LENGTH = 12;
+    public static final int USER_USERNAME_MAX_LENGTH = 64;
+    public static final int USER_PASSWORD_MAX_LENGTH = 64;
+    public static final int USER_PASSWORD_MIN_LENGTH = 12;
 }
