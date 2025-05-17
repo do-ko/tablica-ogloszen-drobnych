40480016659272f074b23ad34e1c0ce31bdec31b
do-ko <doko@onet.eu>
2025-05-17 21:06:29 +0200

    user exception handlers

--- tablica-backend/src/main/java/com/webdevlab/tablicabackend/domain/enums/Role.java
+++ tablica-backend/src/main/java/com/webdevlab/tablicabackend/domain/enums/Role.java
@@ -2,7 +2,7 @@ package com.webdevlab.tablicabackend.domain.enums;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 
-@Schema(description = "User role in the system", allowableValues = {"BUYER", "SELLER"})
+@Schema(description = "User role in the system")
 public enum Role {
     SELLER,
     BUYER
---
+++ tablica-backend/src/main/java/com/webdevlab/tablicabackend/exception/user/UserExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.webdevlab.tablicabackend.exception.user;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.Map;
+
+@ControllerAdvice
+public class UserExceptionHandler {
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<Map<String, String>> handleUserNotFoundException(UserNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Map.of("message", ex.getMessage()));
+    }
+
+    @ExceptionHandler(UserAlreadyExistsException.class)
+    public ResponseEntity<Map<String, String>> handleUserAlreadyExistsException(UserAlreadyExistsException ex) {
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(Map.of("message", ex.getMessage()));
+    }
+
+    @ExceptionHandler(UserRoleException.class)
+    public ResponseEntity<Map<String, String>> handleUserRoleException(UserRoleException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Map.of("message", ex.getMessage()));
+    }
+}
